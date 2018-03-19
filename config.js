eacf1d13023680b29e6a9b5f0fe6bcc3ae950cd4
nisdas <nish1993@hotmail.com>
2018-03-19 13:40:54 +0800

    Editing JS files to keep up with changes in contracts

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var owner;
 var buyer;
 
 module.exports = {
-    address: '0x627306090abaB3A6e1400e9345bC60c78a8BEf57',
+    address: '0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1',
     owner : '0x627306090abaB3A6e1400e9345bC60c78a8BEf57',
     buyer: '0xf17f52151EbEF6C7334FAD080c5704D77216b732'
 
--- deploy.js
+++ deploy.js
@@ -1,34 +1,84 @@
 
-
+var async = require("async");
 var config = require('./config.js');
 var web3 = require("web3");
 var fs = require("fs");
 var solc = require("solc");
 
+/*
+'ECVerify.sol': fs.readFileSync('./contracts/SigLibrary/ECVerify.sol', 'utf8'),
+'SafeMath.sol': fs.readFileSync('./contracts/math/SafeMath.sol', 'utf8'),
+'AssetInterface.sol': fs.readFileSync('./contracts/AssetInterface.sol', 'utf8'),
+*/
+
 web3 = new web3(new web3.providers.HttpProvider("http://localhost:8545"));
-var compiledCode = solc.compile(fs.readFileSync('./contracts/library.sol', 'utf8'),1);
-var source = compiledCode.contracts[":FunctionSet"];
-var source2 = compiledCode.contracts[":Transaction"];
-var LibraryContract = new  web3.eth.Contract(JSON.parse(source.interface));
-var TransactionContract = new  web3.eth.Contract(JSON.parse(source2.interface));
 
-LibraryContract.deploy({
-    data: source.bytecode}).send({
-        from: config.address, gas:1500000}).on('confirmation', function(confirmationNumber, receipt){ 
-            console.log(confirmationNumber); LibraryContract.options.address = receipt.contractAddress; }).on('receipt', function(receipt){
-                console.log(receipt);}).then(function(NxtDeploy){
-                    source2.bytecode = solc.linkBytecode(source2.bytecode, { 'FunctionSet': LibraryContract.options.address });
+function findImports (path) {
+	if (path === 'math/SafeMath.sol')
+        return { contents: fs.readFileSync('./contracts/math/SafeMath.sol', 'utf8') }
+        
+    else if (path === 'SigLibrary/ECVerify.sol')
+    
+    return { contents: fs.readFileSync('./contracts/SigLibrary/ECVerify.sol', 'utf8') }
 
-                    TransactionContract.deploy({
-                        data: source2.bytecode}).send({
+    else if (path === 'AssetInterface.sol')
+
+    return { contents: fs.readFileSync('./contracts/AssetInterface.sol', 'utf8') }
+
+    else 
+		return { error: 'File not found' }
+}
+var input = {
+	'AssetExchange.sol': fs.readFileSync('./contracts/AssetExchange.sol', 'utf8')
+}
+var compiledCode = solc.compile({ sources: input },1, findImports);
+var source = compiledCode.contracts["SigLibrary/ECVerify.sol:ECVerify"];
+var source2 = compiledCode.contracts["math/SafeMath.sol:SafeMath"];
+var source3 = compiledCode.contracts["AssetExchange.sol:AssetExchange"];
+var ECVerifyLibrary = new  web3.eth.Contract(JSON.parse(source.interface));
+var SafeMathLibrary = new  web3.eth.Contract(JSON.parse(source2.interface));
+var AssetExchange = new  web3.eth.Contract(JSON.parse(source3.interface));
+
+function deploySafeMath() {
+    SafeMathLibrary.deploy({
+        data: source2.bytecode}).send({
+            from: config.address, gas:1500000}).on('confirmation', function(confirmationNumber, receipt){ 
+                console.log(confirmationNumber); SafeMathLibrary.options.address = receipt.contractAddress; }).on('receipt', function(receipt){
+                    console.log(receipt);})
+
+}
+
+function deployECVerify() {
+    ECVerifyLibrary.deploy({
+        data: source.bytecode}).send({
+            from: config.address, gas:1500000}).on('confirmation', function(confirmationNumber, receipt){ 
+                console.log(confirmationNumber); ECVerifyLibrary.options.address = receipt.contractAddress; }).on('receipt', function(receipt){
+                    console.log(receipt);})
+
+}
+
+function deployAssetContract() {
+    source3.bytecode = solc.linkBytecode(source3.bytecode, { 'SafeMath': SafeMathLibrary.options.address },{ 'ECVerify': ECVerifyLibrary.options.address });
+
+
+                    AssetExchange.deploy({
+                        data: source3.bytecode}).send({
                             from: config.address, gas:1500000}).on('confirmation', function(confirmationNumber, receipt){ 
-                                console.log(confirmationNumber); TransactionContract.options.address = receipt.contractAddress; }).on('receipt', function(receipt){
+                                console.log(confirmationNumber); AssetExchange.options.address = receipt.contractAddress; }).on('receipt', function(receipt){
                                         console.log(receipt)
                             });
-                })
+
+}
+
+async.waterfall({
+    deploySafeMath,
+    deployECVerify,
+    deployAssetContract
+}, function(error,result){
+    console.log(result);
+})
 
            
-
 
 
 
@@ -38,8 +88,10 @@ module.exports = {
     web3 ,
     source,
     source2,
-    LibraryContract,
-    TransactionContract
+    source3,
+    ECVerifyLibrary,
+    SafeMathLibrary,
+    AssetExchange
     
 
 }
